355afe5980c33943aec0912067aa0610817ada24
unknown <rygailkim@gmail.com>
2023-01-14 11:38:02 +0800

    part 9

--- js/main.js
+++ js/main.js
@@ -29,6 +29,56 @@ function InitFilesRanksBrd() {
 	}
 }
 
+/**
+ * Fills up PieceKeys, SideKey, CastleKeys array with random numbers
+ */
+function InitHashKeys() {
+    var index = 0;
+	
+	for(index = 0; index < 14 * 120; ++index) {				
+		PieceKeys[index] = RAND_32();
+	}
+	
+	SideKey = RAND_32();
+	
+	for(index = 0; index < 16; ++index) {
+		CastleKeys[index] = RAND_32();
+	}
+}
+
+/**
+ * Sets the sq64 and sq arrays
+ */
+function InitSq120To64() {
+
+	var index = 0;
+	var file = FILES.FILE_A;
+	var rank = RANKS.RANK_1;
+	var sq = SQUARES.A1;
+	var sq64 = 0;
+
+	// resets the value
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		Sq120ToSq64[index] = 65;
+	}
+	
+	// resets the value
+	for(index = 0; index < 64; ++index) {
+		Sq64ToSq120[index] = 120;
+	}
+	
+	// loop the board in file-rank order and increment sq64 by 1
+	for(rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+		for(file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+			sq = FR2SQ(file,rank);
+			Sq64ToSq120[sq64] = sq;
+			Sq120ToSq64[sq] = sq64;
+			sq64++;
+		}
+	}
+
+}
+
 function init() {
     console.log("init() called");
     InitFilesRanksBrd();
